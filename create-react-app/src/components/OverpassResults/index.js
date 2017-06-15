1d22093dd1210af15c12108c851fa4b4ebd6bebc
Mathieu DARTIGUES <mathieu@dartic.fr>
2017-06-15 18:12:01 +0200

    [step-5.3] Only one Map component (and replace bind this by fat arrows)

--- create-react-app/src/components/OverpassResults/index.js
+++ create-react-app/src/components/OverpassResults/index.js
@@ -1,38 +1,14 @@
 import React from 'react';
-import overpassService from '../../services/overpass';
-
-import Map from '../Map';
-
 import './style.css';
 
-class OverpassResults extends React.Component {
-
-    constructor(props) {
-        super(props);
-        this.state = {
-            data: []
-        }
-    }
-    
-    componentWillMount() {
-        overpassService.getOverpassData()
-            .then((data) => {
-                this.setState({data: JSON.stringify(data)})
-            });
-    }
-
-    render() {
-        console.log(this.state);
-        return (
-        <div className="OverpassResults">
-            <div className="results">{ JSON.stringify(this.state.data) }</div>
-            {
-            this.state.loading === false &&
-            <Map id="map-overpass" geojson={this.state.data} />
-            }
-        </div>
-        );
-    }
+function OverpassResults({geojson}) {
+  return (
+    <div className="OverpassResults">
+      <div className="results">
+        { JSON.stringify(geojson) }
+      </div>
+    </div>
+  );
 }
 
 export default OverpassResults;
